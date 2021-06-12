bc109272ff088cb4de3dd45e7a7e6f334f4c2e70
john60090012 <48548369+john60090012@users.noreply.github.com>
2021-06-12 19:19:53 +0700

    initial commit

---
+++ src/screens/MenuScreen.js
@@ -0,0 +1,188 @@
+import React from 'react';
+import {Dimensions, SafeAreaView, StyleSheet, View, Text, Image, TextInput, TouchableHighlight,
+  TouchableOpacity,} from 'react-native';
+import {FlatList} from 'react-native-gesture-handler';
+import Icon from 'react-native-vector-icons/MaterialIcons';
+import COLORS from '../consts/colors';
+import { useState, useEffect } from "react";
+import foods from '../consts/foods';
+import {PrimaryButton} from '../components/Button';
+import {DeleteButton} from '../components/Button';
+import {MiniButton} from '../components/Button';
+
+const {width} = Dimensions.get('screen');   //get size of current screen to calculate card width 
+const cardWidth = width - 20;               //card width constant when we have 2 cards per row
+
+const MenuScreen = ({navigation}) => {
+
+  const CategoryapiUrl = window.apiurl + 'api/shops/menucategories/customer/shop/' + navigation.getParam('shopid') + '/';
+
+  const [Category, setCategory] = useState([]);
+  const [Cart, setCart] = useState([]);
+
+
+  useEffect(() => {
+    loadCategoryData();
+  }, []);
+
+  const loadCategoryData = async () => {
+    const response = await fetch(CategoryapiUrl);
+    const data = await response.json();
+    setCategory(data);
+    console.log(data);
+  }
+
+
+  const Card = ({menus}) => {
+    return (
+      <TouchableHighlight
+        underlayColor={COLORS.white}
+        activeOpacity={0.9}
+        onPress={() => {console.log(menus)}}>
+        <View style={style.card}>
+          <View style={{alignItems: 'flex-end', top: 20, left:-20 }}>
+            <img src={menus.main_image} style={{height: 120, width: 120, borderRadius: 15}} />
+          </View>
+          <View style={{marginHorizontal: 20, marginVertical: -80}}>
+            <Text style={{fontSize: 18, fontWeight: 'bold'}}>{menus.name}</Text>
+            <Text style={{fontSize: 16, color: COLORS.grey, marginTop: 2}}>
+              {menus.desc}
+            </Text>
+            <View style={{flexDirection: 'row'}}>
+              <Text style={{fontSize: 18, fontWeight: 'bold'}}>
+                ราคา :<Text>{' '}</Text>
+              </Text>
+              <Text style={{fontSize: 18, fontWeight: 'bold', color: COLORS.primary}}>
+                {menus.price}
+              </Text>
+            </View>
+          </View>
+        </View>
+      </TouchableHighlight>
+    );
+  };
+
+  const Categoryhead = ({categories}) => {
+
+    const [Menu, setMenu] = useState([]);
+
+    useEffect(() => {
+      loadMenuData();
+    }, []);
+  
+
+    const loadMenuData = async () => {
+      const response = await fetch(window.apiurl + 'api/shops/products/customer/menucategory/' + categories.id + '/');
+      const data = await response.json();
+      setMenu(data);
+      console.log(data);
+    }
+
+    return (
+        <>
+            <Text style={{fontSize: 20, fontWeight: 'bold', marginLeft: 20}}>{categories.name}</Text>
+            <FlatList
+              showsVerticalScrollIndicator={false}
+              numColumns={1}
+              data={Menu}
+              renderItem={({item}) => <Card menus={item} />}
+            />
+        </>
+    );
+  };
+
+  if (Category.length == 0) {
+    return (
+    <View style={{backgroundColor: COLORS.white, position: 'absolute', 
+    top: 0, left: 0, 
+    right: 0, bottom: 0, 
+    justifyContent: 'center', 
+    alignItems: 'center'}}>
+      <Text>ไม่พบรายการอาหารใน{navigation.getParam('shopname')}</Text>
+    </View>
+    );
+  }
+
+
+  return (
+    <View style={{flex: 1, backgroundColor: COLORS.white}}>
+      <View style={style.header}>
+        <View>
+          <View style={{flexDirection: 'row', marginTop: 20, }}>
+            <Text style={{fontWeight: 'bold', fontSize: 22, color: COLORS.dark}}>
+              Near
+            </Text>
+            <Text style={{fontWeight: 'bold', fontSize: 22, color: COLORS.primary, marginBottom: 20}}>
+              Me
+            </Text>
+            <Text style={{fontWeight: 'bold', fontSize: 22, color: COLORS.dark, marginBottom: 20, marginLeft: 290}}>
+            </Text>
+          </View>
+            <View style={{flexDirection: 'row'}}>
+              <Text style={{fontSize: 22, fontWeight: 'bold'}}>กรุณาเลือกรายการอาหารภายใน</Text>
+            </View>
+            <Text style={{marginTop: 5, fontSize: 22, color: COLORS.primary}}>
+              {navigation.getParam('shopname')}
+            </Text>
+        </View>  
+      </View>
+
+      <FlatList
+        showsVerticalScrollIndicator={false}
+        numColumns={1}
+        data={Category}
+        renderItem={({item}) => <Categoryhead categories={item} />}
+      />
+    </View>
+  );
+};
+
+const style = StyleSheet.create({
+  header: {
+    marginTop: 20,
+    flexDirection: 'row',
+    justifyContent: 'space-between',
+    paddingHorizontal: 20,
+  },
+  card: {
+    height: 160,
+    width: cardWidth,
+    marginHorizontal: 10,
+    marginBottom: 10,
+    marginTop: 10,
+    borderRadius: 15,
+    elevation: 13,
+    backgroundColor: COLORS.light,
+  },
+  inputContainer: {
+    flex: 1,
+    height: 50,
+    borderRadius: 10,
+    flexDirection: 'row',
+    backgroundColor: COLORS.light,
+    alignItems: 'center',
+    paddingHorizontal: 20,
+  },
+  cartBtn: {
+    width: 140,
+    height: 50,
+    marginLeft: 10,
+    backgroundColor: COLORS.primary,
+    borderRadius: 10,
+    justifyContent: 'center',
+    alignItems: 'center',
+    flexDirection: 'row',
+  },
+  addToFavoriteBtn: {
+    height: 35,
+    width: 35,
+    borderRadius: 20,
+    backgroundColor: COLORS.primary,
+    justifyContent: 'center',
+    alignItems: 'center',
+    marginLeft: 125,
+    marginTop: 20,
+  },// Margin left is added later
+});
+
+export default MenuScreen;
---
+++ src/screens/OnBoardScreen.js
@@ -0,0 +1,86 @@
+import React from 'react';
+import {Text, StyleSheet, View, Image} from 'react-native';
+import {SafeAreaView} from 'react-native-safe-area-context';
+import COLORS from '../consts/colors';
+import {PrimaryButton} from '../components/Button';
+
+const OnBoardScreen = ({navigation}) => {
+  return (
+    <SafeAreaView style={{flex: 1, backgroundColor: COLORS.white}}>
+      <View style={{height: 400}}>
+        <Image
+          style={{
+            width: '100%',
+            resizeMode: 'contain',
+            top: -150,
+          }}
+          source={require('../assets/cheesePizza.png')}
+        />
+      </View>
+      <View style={style.textContainer}>
+        <View>
+          <Text style={{fontSize: 32, fontWeight: 'bold', textAlign: 'center'}}>
+            Near Me Delivery Service
+          </Text>
+          <Text
+            style={{
+              marginTop: 20,
+              fontSize: 18,
+              textAlign: 'center',
+              color: COLORS.grey,
+            }}>
+            หาร้านอาหารใกล้ตัวคุณได้ที่เรา
+          </Text>
+        </View>
+        <View style={style.indicatorContainer}>
+          <View style={style.currentIndicator} />
+          <View style={style.indicator} />
+          <View style={style.indicator} />
+        </View>
+        <PrimaryButton
+          onPress={() => navigation.navigate('Sign')}
+          title="สมัครเข้าใช้"
+        />
+        
+        <View style={{marginBottom:20}}></View>
+
+        <PrimaryButton
+          onPress={() => navigation.navigate('Login')}
+          title="ลงชื่อเข้าใช้"
+        />
+      </View>
+    </SafeAreaView>
+  );
+};
+
+const style = StyleSheet.create({
+  textContainer: {
+    flex: 1,
+    paddingHorizontal: 50,
+    justifyContent: 'space-between',
+    paddingBottom: 40,
+  },
+  indicatorContainer: {
+    height: 50,
+    flex: 1,
+    justifyContent: 'center',
+    flexDirection: 'row',
+    alignItems: 'center',
+  },
+  currentIndicator: {
+    height: 12,
+    width: 30,
+    borderRadius: 10,
+    backgroundColor: COLORS.primary,
+    marginHorizontal: 5,
+  },
+  indicator: {
+    height: 12,
+    width: 12,
+    borderRadius: 6,
+    backgroundColor: COLORS.grey,
+    marginHorizontal: 5,
+  },
+});
+
+export default OnBoardScreen;
